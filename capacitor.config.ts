370f69c4fe91cd6804ba67ee80ba79a93631ac90
AYESA\maiglesiasp <iglesiasarroyop@gmail.com>
2026-05-12 18:07:05 +0200

    Cambiado capacitor.config.ts por .json para evitar error de TypeScript

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.guajinos.sls',
-  appName: 'Super Liquid Soccer',
-  webDir: 'www'
-};
-
-export default config;
